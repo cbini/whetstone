cad43bc9870670bb35b92a93e2543c41697e7f5a
Charlie Bini <cbini87@gmail.com>
2019-11-26 16:36:08 -0600

    adding client class

--- whetstone/whetstone.py
+++ whetstone/whetstone.py
@@ -1,3 +1,22 @@
 # -*- coding: utf-8 -*-
 
 """Main module."""
+
+import requests
+
+class Client:
+    def __init__(self, api_key):
+        self.base_url = 'https://app.whetstoneeducation.com'
+        self.session = requests.Session()
+        self.session.headers['x-key'] = api_key
+
+    def auth(self):
+        api_auth_url = f'{self.base_url}/auth/api'
+        api_auth_payload = {
+            'apikey': self.session.headers['x-key'],
+        }
+
+        api_auth_response = self.session.post(url=api_auth_url, data=api_auth_payload)
+        
+        api_auth_response_dict = api_auth_response.json()
+        self.session.headers['x-access-token'] = api_auth_response_dict.get('token')
